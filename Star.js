16fbe66994f20e7e2ed72756910fc2f983f55cf8
Ahm3dGlitch <81803344+Ahm3dGlitch@users.noreply.github.com>
2023-02-05 12:41:25 +0600

    Update Star.js

--- Star.js
+++ Star.js
@@ -4,6 +4,9 @@ const fs = require("fs");
 
 const url = "https://www.startech.com.bd";
 const prod_Data = []
+let name;
+let regularprice;
+let offerprice;
 async function getPdata(){
     try{
         const response = await axios.get(url);
@@ -11,20 +14,31 @@ async function getPdata(){
         
         const products = $("div.p-item");
         products.each(function(){
-            Name = $(this).find("h4 a").text();
-            Price = $(this).find("div.p-item-price").text().trim();
-
-            prod_Data.push({Name,Price});
+            name = $(this).find("h4 a").text();
+            //anchorlink = $(this).find("h4 a").attr('href');
+            //imagelink = $(this).find("a img").attr('src');
+            price = $(this).find("span.price-new").text();
+            console.log(price);
+            if($(this).find("span.price-new").data()!==null){
+            
+                regularprice = $(this).find("span.price-old").text().trim();
+                offerprice = $(this).find("span.price-new").text().trim();
+                prod_Data.push(`Name: ${name}`,`Offer Price: ${offerprice}`,`Regular Price: ${regularprice}`);
+            }
+            else{
+                regularprice = $(this).find("div.p-item-price").text().trim();
+                prod_Data.push(`Name: ${name}`,`Price: ${regularprice}`);
+            }
               
         });
         console.log(prod_Data);
-        fs.writeFile("Star.json", JSON.stringify(prod_Data, null, 2), (err) => {
+        /*fs.writeFile("Star.json", JSON.stringify(prod_Data, null, 2), (err) => {
             if (err) {
               console.error(err);
               return;
             }
             console.log("Successfully written data to file");
-        });
+        });*/
     }
     catch(error){
         console.error(error);
