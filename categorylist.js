481359044d3354cd984824148dd937abd9fadc29
Ahm3dGlitch <81803344+Ahm3dGlitch@users.noreply.github.com>
2023-02-13 19:02:44 +0600

    Add files via upload

---
+++ categorylist.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const url = 'https://www.chaldal.com/';
+
+request(url, (error, response, html) => {
+  if (!error && response.statusCode == 200) {
+    const $ = cheerio.load(html);
+    const categories = [];
+
+    // Find the category elements and extract the text
+    $('.level-0 li a').each((i, el) => {
+      const category = $(el).text().trim();
+      categories.push(category);
+    });
+    fs.writeFile("coutries.json", JSON.stringify(categories, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("Successfully written data to file");
+    });
+
+    console.log(categories);
+  }
+});
